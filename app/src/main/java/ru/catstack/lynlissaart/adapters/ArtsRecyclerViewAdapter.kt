32e6c021416af50a9ca62553d98f3ebf410db330
Gorr <gorrgorrob@gmail.com>
2020-02-01 20:44:27 +0500

    Glide loading

--- app/src/main/java/ru/catstack/lynlissaart/adapters/ArtsRecyclerViewAdapter.kt
+++ app/src/main/java/ru/catstack/lynlissaart/adapters/ArtsRecyclerViewAdapter.kt
@@ -5,12 +5,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.marginBottom
 import androidx.core.view.marginEnd
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.list_item_view.view.*
 import ru.catstack.lynlissaart.R
 import ru.catstack.lynlissaart.domain.Art
 
-class ArtsRecyclerViewAdapter(private val arts: ArrayList<Art>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class ArtsRecyclerViewAdapter(private val arts: ArrayList<Art>, private val fragment: Fragment) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun getItemCount() = arts.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
@@ -22,6 +24,7 @@ class ArtsRecyclerViewAdapter(private val arts: ArrayList<Art>) : RecyclerView.A
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val viewHolder = holder as ViewHolder
+        Glide.with(fragment).load(arts[position].imageUrl).into(viewHolder.imageView)
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
--- app/src/main/java/ru/catstack/lynlissaart/ui/main/MainFragment.kt
+++ app/src/main/java/ru/catstack/lynlissaart/ui/main/MainFragment.kt
@@ -27,8 +27,9 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         recyclerView.addItemDecoration(GridSpacingItemDecoration(2, TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8f, resources.displayMetrics).toInt()))
-        // TODO: Use the ViewModel
-        recyclerView.adapter = ArtsRecyclerViewAdapter(arrayListOf(Art("", "", ""),
+        recyclerView.adapter = ArtsRecyclerViewAdapter(arrayListOf(
+            Art("", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7b351c1a-730a-4786-a910-7bb786aa7117/ddp2ypk-60bf051e-8187-4c58-928e-a7efe93d0915.png/v1/fill/w_1102,h_725,q_70,strp/key_keeper_by_lynlissa_ddp2ypk-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODQzIiwicGF0aCI6IlwvZlwvN2IzNTFjMWEtNzMwYS00Nzg2LWE5MTAtN2JiNzg2YWE3MTE3XC9kZHAyeXBrLTYwYmYwNTFlLTgxODctNGM1OC05MjhlLWE3ZWZlOTNkMDkxNS5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.7Fn7oqODgKJZUop5qj5KKQTxFZ4MKRWTGdElr8fHSI0", ""),
+            Art("", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7b351c1a-730a-4786-a910-7bb786aa7117/ddp2ygo-7443476a-1cf8-4bf5-9d7b-fc470fc51edd.png/v1/fill/w_1123,h_711,strp/wardrobe_of_young_squire_by_lynlissa_ddp2ygo-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODExIiwicGF0aCI6IlwvZlwvN2IzNTFjMWEtNzMwYS00Nzg2LWE5MTAtN2JiNzg2YWE3MTE3XC9kZHAyeWdvLTc0NDM0NzZhLTFjZjgtNGJmNS05ZDdiLWZjNDcwZmM1MWVkZC5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.FfBJBUVhaRBV-GOxVIPZ5KqeyuuSuyFgtZPNUbIM1sg", ""),
             Art("", "", ""),
             Art("", "", ""),
             Art("", "", ""),
@@ -44,9 +45,7 @@ class MainFragment : Fragment() {
             Art("", "", ""),
             Art("", "", ""),
             Art("", "", ""),
-            Art("", "", ""),
-            Art("", "", ""),
-            Art("", "", "")))
+            Art("", "", "")), this)
         viewModel.foo()
     }
 
