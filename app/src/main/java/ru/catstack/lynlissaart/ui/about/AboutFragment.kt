6718e6024c04f679161fbee643a1e5c3176b86ca
Gorr <gorrgorrob@gmail.com>
2020-02-03 16:07:25 +0500

    Icon update, russian translation & about fix

--- app/src/main/java/ru/catstack/lynlissaart/ui/about/AboutFragment.kt
+++ app/src/main/java/ru/catstack/lynlissaart/ui/about/AboutFragment.kt
@@ -1,11 +1,14 @@
 package ru.catstack.lynlissaart.ui.about
 
+import android.content.Intent
 import android.content.pm.ActivityInfo
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.about_fragment.*
 import ru.catstack.lynlissaart.R
 
 class AboutFragment : Fragment() {
@@ -19,5 +22,26 @@ class AboutFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+        vkButton.setOnClickListener {
+            openLink("https://vk.com/lynlissa_art")
+        }
+
+        instagramButton.setOnClickListener {
+            openLink("https://www.instagram.com/lynlissa_art")
+        }
+
+        deviantartButton.setOnClickListener {
+            openLink("https://www.deviantart.com/lynlissa")
+        }
+
+        twitterButton.setOnClickListener {
+            openLink("https://twitter.com/Hurrrsky")
+        }
+    }
+
+    private fun openLink(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
     }
 }
